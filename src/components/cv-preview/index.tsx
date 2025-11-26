8403f964443766522fd0ee1646d82ec5559849fe
qileli <qileli@tencent.com>
2025-11-26 16:58:57 +0800

    feat: 暂存

---
+++ src/components/cv-preview/index.tsx
@@ -0,0 +1,12 @@
+import { type FC } from 'react';
+
+const CVPreview: FC = () => {
+  return (
+    <div>
+      <h1>CV Preview</h1>
+      <p>This is a preview of the CV.</p>
+    </div>
+  );
+};
+
+export default CVPreview;
